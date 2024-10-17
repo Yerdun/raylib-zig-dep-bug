7df0d923b20fb17ac119e95c32a1e64b9cbfd97e
Jordan <56048350+Yerdun@users.noreply.github.com>
2024-10-17 04:19:53 -0700

    Initial commit made sample project, currently untested since zig doesn't build right now

---
+++ build.zig
@@ -0,0 +1,35 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) !void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const raylib_dep =  b.dependency("raylib", .{
+        .target = target,
+        .optimize = optimize,
+        .config = @as([]const u8, "-DSUPPORT_CUSTOM_FRAME_CONTROL -DSUPPORT_FILEFORMAT_FLAC"),
+    });
+    const raylib = raylib_dep.artifact("raylib");
+
+    const exe = b.addExecutable(.{
+        .name = "raylib-zig-dep-bug",
+        .target = target,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(raylib);
+    exe.linkLibrary(raylib);
+
+    exe.linkLibC();
+
+    exe.addCSourceFiles(.{
+        .files = &.{"example.c"},
+        .flags = &.{"-lraylib"},
+    });
+
+    b.installArtifact(exe);
+
+    const run_exe = b.addRunArtifact(exe);
+    const run_step = b.step("run", "Run the application");
+    run_step.dependOn(&run_exe.step);
+}
